1d84c2ed98f2751e87bf782d1c0e912339de8575
unknown <tonyekim>
2023-11-24 17:32:39 +0100

    change to base_url

--- src/App.jsx
+++ src/App.jsx
@@ -52,7 +52,7 @@ function App() {
   setInterval(clearLocalStorage, interval);
 
   // axios configs
-  axios.defaults.baseURL = base_url_local;
+  axios.defaults.baseURL = base_url;
   axios.defaults.headers.post["Content-Type"] = "application/json";
   axios.defaults.headers.common["Authorization"] = `Bearer ${user?.token}`;
 
